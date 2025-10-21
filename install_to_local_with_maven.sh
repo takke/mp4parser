927ffd5ff5c20178599aa407350e39811c3c85b6
takke <takke30@gmail.com>
2025-10-21 14:07:18 +0900

    mac環境用に修正

--- install_to_local_with_maven.sh
+++ install_to_local_with_maven.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-export JAVA_HOME=/c/Users/takke/.jdks/corretto-1.8.0_292
+#export JAVA_HOME=/c/Users/takke/.jdks/corretto-1.8.0_292
+export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
 mvn clean install -Dmaven.test.skip=true
